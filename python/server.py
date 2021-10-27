7c8e48429ffe07d9044223382c394701e06138e1
Zelenov Anton <tixset@mail.ru>
2021-10-27 23:36:42 +0300

    Update server.py

--- python/server.py
+++ python/server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 import socket
 from random import randint
  
@@ -9,38 +10,43 @@ port = 5555
 
 sharedKey = 0
 privateKey = 0
-publicKey = 0
-KeyCipher = 0
+clientPublicKey = 0
+serverPublicKey = 0
+keyCipher = 0
 
 sock = socket.socket()
-sock.bind((host, port))
+try:
+    sock.bind((host, port))
+except Exception:
+    print "Не могу открыть сокет!"
+    sys.exit(1)
 sock.listen(1)
 
 print "Сервер запущен"
 
 while True:
-    client_socket, addr = sock.accept()
+    clientSocket, addr = sock.accept()
     while True:
-        recieved_data = client_socket.recv(1024)
-        if not recieved_data:
+        recievedData = clientSocket.recv(1024)
+        if not recievedData:
             break
-        data_arr = recieved_data.split(':')
-        if recieved_data == "GET_SHARED_KEY":
+        dataArr = recievedData.split(':')
+        if recievedData == "GET_SHARED_KEY":
             print "Клиент запрашивает общий ключ"
             sharedKey = randint(1000, 9999)
             print "Общий ключ сгенерирован: " + str(sharedKey)
             print "Отправляю общий ключ: " + str(sharedKey)
-            client_socket.send("SHARED_KEY:" + str(sharedKey))
-        if (data_arr[0]) and (data_arr[0] == "PUBLIC_KEY"):
-            publicKey = int(data_arr[1])
-            print "Публичный ключ клиента получен: " + str(publicKey)
+            clientSocket.send("SHARED_KEY:" + str(sharedKey))
+        if (dataArr[0]) and (dataArr[0] == "PUBLIC_KEY"):
+            clientPublicKey = int(dataArr[1])
+            print "Публичный ключ клиента получен: " + str(clientPublicKey)
             privateKey = randint(1000, 9999);
             print "Приватный ключ сгенерирован: " + str(privateKey)
-            KeyCipher = publicKey + privateKey
-            print "> Ключ шифрования посчитан: " + str(KeyCipher)
-            publicKey = sharedKey + privateKey
-            print "Публичный ключ сервера посчитан: " + str(publicKey)
-            print "Отправляю публичный ключ: " + str(publicKey)
-            client_socket.send("PUBLIC_KEY:" + str(publicKey))
+            keyCipher = clientPublicKey + privateKey
+            print "> Ключ шифрования посчитан: " + str(keyCipher)
+            serverPublicKey = sharedKey + privateKey
+            print "Публичный ключ сервера посчитан: " + str(serverPublicKey)
+            print "Отправляю публичный ключ: " + str(serverPublicKey)
+            clientSocket.send("PUBLIC_KEY:" + str(serverPublicKey))
             print "Задача завершена!"
             break
