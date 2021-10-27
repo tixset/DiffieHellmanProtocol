d73cfd682765274e504f63564a8df1d38769e6bc
Zelenov Anton <tixset@mail.ru>
2021-10-27 23:33:58 +0300

    Update client.py

--- python/client.py
+++ python/client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 import socket
 from random import randint
 
@@ -9,33 +10,38 @@ port = 5555
 
 sharedKey = 0
 privateKey = 0
-publicKey = 0
-KeyCipher = 0
+clientPublicKey = 0
+serverPublicKey = 0
+keyCipher = 0
 
 socket = socket.socket()
-socket.connect((host, port))
+try:
+    socket.connect((host, port))
+except Exception:
+    print "Не могу подключиться к серверу!"
+    sys.exit(1)
 
 print "Клиент запущен"
 print "Запрашиваю общий ключ"
 socket.send("GET_SHARED_KEY")
 
 while True:
-    recieved_data = socket.recv(1024)
-    data_arr = recieved_data.split(':')
-    if data_arr[0] == "SHARED_KEY":
-        sharedKey = int(data_arr[1])
+    recievedData = socket.recv(1024)
+    dataArr = recievedData.split(':')
+    if dataArr[0] == "SHARED_KEY":
+        sharedKey = int(dataArr[1])
         print "Общий ключ получен: " + str(sharedKey)
         privateKey = randint(1000, 9999)
         print "Приватный ключ сгенерирован: " + str(privateKey)
-        publicKey = sharedKey + privateKey
-        print "Публичный ключ клиента посчитан: " + str(publicKey)
-        print "Отправляю публичный ключ: " + str(publicKey)
-        socket.send("PUBLIC_KEY:" + str(publicKey))
-    if (data_arr[0]) and (data_arr[0] == "PUBLIC_KEY"):
-        publicKey = int(data_arr[1])
-        print "Публичный ключ сервера получен: " + str(publicKey)
-        KeyCipher = publicKey + privateKey
-        print "> Ключ шифрования посчитан: " + str(KeyCipher)
+        clientPublicKey = sharedKey + privateKey
+        print "Публичный ключ клиента посчитан: " + str(clientPublicKey)
+        print "Отправляю публичный ключ: " + str(clientPublicKey)
+        socket.send("PUBLIC_KEY:" + str(clientPublicKey))
+    if (dataArr[0]) and (dataArr[0] == "PUBLIC_KEY"):
+        serverPublicKey = int(dataArr[1])
+        print "Публичный ключ сервера получен: " + str(serverPublicKey)
+        keyCipher = serverPublicKey + privateKey
+        print "> Ключ шифрования посчитан: " + str(keyCipher)
         print "Задача завершена!"
 	socket.close()
-	break
+	sys.exit()
