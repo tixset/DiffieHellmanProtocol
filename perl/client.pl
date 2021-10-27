0100a4727472e90426a89021765aa722746fd10a
Zelenov Anton <tixset@mail.ru>
2021-10-27 23:13:53 +0300

    Update client.pl

--- perl/client.pl
+++ perl/client.pl
@@ -7,10 +7,11 @@ my $port = 5555;
 
 my $sharedKey;
 my $privateKey;
-my $publicKey;
-my $KeyCipher;
-my @data_arr;
-my $recv_data;
+my $clientPublicKey;
+my $serverPublicKey;
+my $keyCipher;
+my @dataArr;
+my $recvData;
 
 sub getRandInt{
     my ($min, $max) = (shift, shift);
@@ -18,30 +19,30 @@ sub getRandInt{
 }
 
 my $socket = new IO::Socket::INET (PeerAddr => $host, PeerPort => $port, Proto => 'tcp');
-die "Не могу открыть сокет!\n" unless $socket;
+die "Не могу подключиться к серверу!\n" unless $socket;
 
 print "Клиент запущен\n";
 print "Запрашиваю общий ключ\n";
 $socket->send("GET_SHARED_KEY");
 
 while (1) {
-    $socket->recv($recv_data, 1024);
-    @data_arr = split(':', $recv_data);
-    if ($data_arr[0] eq "SHARED_KEY") {
-        $sharedKey = $data_arr[1];
+    $socket->recv($recvData, 1024);
+    @dataArr = split(':', $recvData);
+    if ($dataArr[0] eq "SHARED_KEY") {
+        $sharedKey = $dataArr[1];
         print "Общий ключ получен: $sharedKey\n";
         $privateKey = getRandInt(1000, 9999);
         print "Приватный ключ сгенерирован: $privateKey\n";
-        $publicKey = $sharedKey + $privateKey;
-        print "Публичный ключ клиента посчитан: $publicKey\n";
-	print "Отправляю публичный ключ: $publicKey\n";
-        $socket->send("PUBLIC_KEY:$publicKey");
+        $clientPublicKey = $sharedKey + $privateKey;
+        print "Публичный ключ клиента посчитан: $clientPublicKey\n";
+	print "Отправляю публичный ключ: $clientPublicKey\n";
+        $socket->send("PUBLIC_KEY:$clientPublicKey");
     }
-    if ((defined $data_arr[0]) && ($data_arr[0] eq "PUBLIC_KEY")) {
-        $publicKey = $data_arr[1];
-	print "Публичный ключ сервера получен: $publicKey\n";
-        $KeyCipher = $publicKey + $privateKey;
-        print "> Ключ шифрования посчитан: $KeyCipher\n";
+    if ((defined $dataArr[0]) && ($dataArr[0] eq "PUBLIC_KEY")) {
+        $serverPublicKey = $dataArr[1];
+	print "Публичный ключ сервера получен: $serverPublicKey\n";
+        $keyCipher = $serverPublicKey + $privateKey;
+        print "> Ключ шифрования посчитан: $keyCipher\n";
         print "Задача завершена!\n";
 	close($socket);
         exit;
